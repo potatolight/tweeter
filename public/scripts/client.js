fa00df6bb918fa47ce5744109bb602e7015cec85
potatolight <shuguangbai@gmail.com>
2020-08-14 00:07:48 +0000

    add media part

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,28 +2,29 @@ const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const renderTweets = function(tweets) {
 
   const sortedTweets = tweets.sort(function(a, b){ 
     return a.created_at - b.created_at;
-  })
+  });
   let tweetContainer = $('.tweet-container');
   tweetContainer.empty();
   for (const tweet of sortedTweets) {
     let tweetElement = createTweetElement(tweet);
     tweetContainer.prepend(tweetElement);
   }
-}
+};
 
 const createTweetElement = function(tweet) {
-  const realName = tweet.user.name ;
-  const userName = tweet.user.handle ;
-  const avatar = tweet.user.avatars ;
-  const text = tweet.content.text ;
+  const realName = tweet.user.name;
+  const userName = tweet.user.handle;
+  const avatar = tweet.user.avatars;
+  const text = tweet.content.text;
   const msecond = tweet.created_at;
-  const day = Math.floor(msecond/86400000)
+  const days = whenLeaveMessage(msecond);
+ 
   let html = `<div class="ind">
          <header>
             <ul>
@@ -40,7 +41,7 @@ const createTweetElement = function(tweet) {
           </article> 
           <footer>
             <ul>
-              <li class="days">${day}days</li>
+              <li class="days">${days}</li>
               <div class="flag">
                 <li><i class="fab fa-gratipay"></i></li>
                 <li><i class="fas fa-retweet"></i></li>
@@ -53,51 +54,51 @@ const createTweetElement = function(tweet) {
 
   let tweetElement = html;
   return tweetElement;
-}
-
-
+};
+const whenLeaveMessage = function(msecond) {
+  let d = new Date();
+  let n = d.getTime();
+  const day = Math.floor((n - msecond) / 86400000);
+  if (day > 1) {
+    return day + " days";
+  } else if (day === 1) {
+    return day + " day";
+  } else {
+    return "Today";
+  }
+};
 
 $( document ).ready(function() {
+  const loadtweets = function() {
+    $.getJSON(`/tweets`)
+      .then((tweets) => {
+        renderTweets(tweets);
+      });
+  };
+    
 
-    function loadtweets() {
-      $.getJSON(`/tweets`)
-        .then((tweets) => {
-          console.log(tweets);
-          renderTweets(tweets)
-        });
+  $('#new-tweet').on('submit', function(event) {
+    event.preventDefault();
+    if ($(".tweet-text").val().length === 0) {
+      $(".firstdiv").slideDown("slow");
+      $(".firstdiv").text("(>_<) No Comment!");
+    } else if ($(".tweet-text").val().length > 140) {
+      $(".firstdiv").slideDown("slow");
+      $(".firstdiv").text("(>人<)Longer Than 140 charactor!");
+      return;
+    } else {
+      $(".firstdiv").hide();
     }
 
-
-
-    $('#new-tweet').on('submit', function(event) {
-      event.preventDefault();
-      if($(".tweet-text").val().length === 0) {
-        $(".firstdiv").slideDown("slow")
-        return
-      } 
-      // else if ($(".tweet-text").val().length >140) {
-      //   $(".seconddiv").slideDown("slow")
-      //   return
-      // }
-      
-       else {
-        $(".firstdiv").hide();
-      } 
-    
-      const serialized = $(this).serialize();
-      console.log('serialized', serialized);
-
-     
-      $.post('/tweets', serialized)
-        .then(() => {
-        // console.log(tweet)
-        
-            loadtweets();
- 
-        })
-    })
-
-    loadtweets();
-        
+    const serialized = $(this).serialize();
+    console.log('serialized', serialized);
+    $.post('/tweets', serialized)
+      .then(() => {
+        loadtweets();
+        $(".tweet-text").val("");
+        $(".counter").val(140);
+      });
+  });
+  loadtweets();
 });
 
